ea97b4110ef86d85d6dc60d6aecb5dc3045a85b3
exe1vy <1meluvfri1@gmail.com>
2026-03-06 08:34:23 -0500

    jiohuyvtbu

---
+++ Slippery Situation - FLaT/Assets/TutorialInfo/Scripts/slippery.cs
@@ -0,0 +1,114 @@
+using UnityEngine;
+
+public class slippery : MonoBehaviour
+{
+ [Header("Movement")]
+    public float moveSpeed;
+    public float groundDrag;
+    public float driftFactor = 0.9f;
+
+    public float jumpForce;
+    public float jumpCooldown;
+    public float airMultiplier;
+    bool readyToJump;
+
+    [HideInInspector] public float walkSpeed;
+    [HideInInspector] public float sprintSpeed;
+
+    [Header("Keybinds")]
+    public KeyCode jumpKey = KeyCode.Space;
+
+    [Header("Ground Check")]
+    public float playerHeight;
+    public LayerMask whatIsGround;
+    bool grounded;
+
+    float horizontalInput;
+    float verticalInput;
+
+    Vector3 moveDirection;
+
+    Rigidbody rb;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        rb.freezeRotation = true;
+
+        readyToJump = true;
+    }
+
+    private void Update()
+    {
+        grounded = Physics.Raycast(transform.position, Vector3.down, playerHeight * 0.5f + 0.3f, whatIsGround);
+
+        MyInput();
+        SpeedControl();
+
+        if (grounded)
+            rb.linearDamping = groundDrag;
+        else
+            rb.linearDamping = 0;
+    }
+
+    private void FixedUpdate()
+    {
+        MovePlayer();
+    }
+
+    private void MyInput()
+    {
+        horizontalInput = Input.GetAxisRaw("Horizontal");
+        verticalInput = Input.GetAxisRaw("Vertical");
+
+        if(Input.GetKey(jumpKey) && readyToJump && grounded)
+        {
+            readyToJump = false;
+
+            Jump();
+
+            Invoke(nameof(ResetJump), jumpCooldown);
+        }
+    }
+
+    private void MovePlayer()
+    {
+         moveDirection = transform.forward * verticalInput + transform.right * horizontalInput;
+
+        float multiplier = grounded ? 1f : airMultiplier;
+
+        Vector3 targetForce = moveDirection.normalized * moveSpeed * 10f * multiplier;
+
+        rb.AddForce(targetForce, ForceMode.Force);
+
+        Vector3 velocity = rb.linearVelocity;
+        Vector3 forwardVel = transform.forward * Vector3.Dot(velocity, transform.forward);
+        Vector3 sidewaysVel = transform.right * Vector3.Dot(velocity, transform.right);
+
+        sidewaysVel *= driftFactor;
+
+        rb.linearVelocity = forwardVel + sidewaysVel + Vector3.up * velocity.y;
+    }
+
+    private void SpeedControl()
+    {
+        Vector3 flatVel = new Vector3(rb.linearVelocity.x, 0f, rb.linearVelocity.z);
+
+        if(flatVel.magnitude > moveSpeed)
+        {
+            Vector3 limitedVel = flatVel.normalized * moveSpeed;
+            rb.linearVelocity = new Vector3(limitedVel.x, rb.linearVelocity.y, limitedVel.z);
+        }
+    }
+
+    private void Jump()
+    {
+        rb.linearVelocity = new Vector3(rb.linearVelocity.x, 0f, rb.linearVelocity.z);
+
+        rb.AddForce(transform.up * jumpForce, ForceMode.Impulse);
+    }
+    private void ResetJump()
+    {
+        readyToJump = true;
+    }
+}
