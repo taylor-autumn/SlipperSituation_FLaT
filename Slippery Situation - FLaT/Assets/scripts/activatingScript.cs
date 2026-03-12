4582ab2966f6ab60994b2122ac86c02727f91aac
exe1vy <1meluvfri1@gmail.com>
2026-03-12 14:40:36 -0400

    dfvhbjkjnlm;

--- Slippery Situation - FLaT/Assets/scripts/activatingScript.cs
+++ Slippery Situation - FLaT/Assets/scripts/activatingScript.cs
@@ -1,20 +1,71 @@
 using UnityEngine;
+using TMPro;
+using System.Collections;
 
 public class activatingScript : MonoBehaviour
 {
-    public GameObject dial;
+    public TextMeshProUGUI textComponent;
+    public string[] lines;
+    public int finalLine;
+    public float textSpeed;
+    private int index;
+
     void OnTriggerEnter(Collider other)
     {
         if (other.gameObject.CompareTag("Player"))
         {
-            dial.SetActive(true);
+            //reference the player controller and disable it
+            
+            Invoke("dialingit", .5f);
         }
     }
-    void OgerExit(Collider other)
+    void StartDialougue()
     {
-        if (other.gameObject.CompareTag("Player"))
+        index = 0;
+        StartCoroutine(TypeLine());
+    }
+
+    void Start(){
+
+        textComponent.text = string.Empty;
+        StartDialougue();
+    }
+
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
         {
-            dial.SetActive(false);
+            if (textComponent.text == lines[index])
+            {
+                NextLine();
+            }
+            else
+            {
+                StopAllCoroutines();
+                textComponent.text = lines[index];
+            }
+        }
+    }
+
+    IEnumerator TypeLine()
+    {
+        foreach (char c in lines[index].ToCharArray())
+        {
+            textComponent.text += c;
+            yield return new WaitForSeconds(textSpeed);
+        }
+    }
+    void NextLine()
+    {
+        if (index < finalLine - 1)
+        {
+            index++;
+            textComponent.text = string.Empty;
+            StartCoroutine(TypeLine());
+        }
+        else
+        {
+          index = Random.Range(finalLine, lines.Length);
         }
     }
 }
