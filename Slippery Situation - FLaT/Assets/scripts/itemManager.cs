f0b2587f10c00933551dc2bc31b9f4d34abaca86
exe1vy <1meluvfri1@gmail.com>
2026-04-06 22:37:38 -0400

    ebwinom

--- Slippery Situation - FLaT/Assets/scripts/itemManager.cs
+++ Slippery Situation - FLaT/Assets/scripts/itemManager.cs
@@ -1,6 +1,9 @@
 using UnityEngine;
 using System.Collections.Generic;
 using UnityEngine.SceneManagement;
+using UnityEngine.UIElements;
+using Unity.VisualScripting;
+using System;
 
 public class itemManager : MonoBehaviour
 {
@@ -75,6 +78,12 @@ public class itemManager : MonoBehaviour
             if (changering < 1)
                 {
                     changering += 0.0008f;
+                    
+                }
+            float slowingdown = playerRef.groundDrag += 0.0008f;
+            if (slowingdown == ogDrag + 10f)
+                {
+                    playerRef.groundDrag = ogDrag + 10f;
                 }
             Color lerpingit = Color.Lerp(normColor, mudColor, changering);
             myMaterial.color = lerpingit;
@@ -84,6 +93,12 @@ public class itemManager : MonoBehaviour
             if (changering > 0)
                 {
                     changering -= 0.0008f;
+                    
+                }
+            float speedingup = playerRef.groundDrag -= 0.005f;
+                    if (speedingup < ogDrag + 0.05)
+                {
+                    playerRef.groundDrag = ogDrag;
                 }
             Color lerpingit = Color.Lerp(normColor, mudColor, changering);
             myMaterial.color = lerpingit;
